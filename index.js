1abbe00631ab79b3d685f55b0536d52cda9b6a0a
shubhamgpta7 <shubhamgpta7@gmail.com>
2022-06-10 10:39:49 +0530

    Started FrontEnd Part

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 //importing dependencies for file upload
-var fileUpload =  require("express-fileupload");
+//var fileUpload =  require("express-fileupload");
 //importing file system
-var fs = require("fs");
+//var fs = require("fs");
 //instantiating file upload
-app.use(fileUpload);
-app.use('/EmployeePic', Express.static(__dirname + '/EmployeePic'));
+//app.use(fileUpload);
+//app.use('/EmployeePic', Express.static(__dirname + '/EmployeePic'));
 
 
 //Enable CORS 
@@ -161,14 +161,14 @@ app.delete('/api/department/:departmentId', (request, response)=>{
 })
 
 //function to upload a file
-app.post('/api/employee/savefile', (request, response)=>{
-    fs.writeFile("./EmployeePic/"+request.files.file.name,
-    request.files.file.data, function(err){
-        if(err)
-            console.log(err);
-        response.send(request.files.file.name);
-    })
-})
+// app.post('/api/employee/savefile', (request, response)=>{
+//     fs.writeFile("./EmployeePic/"+request.files.file.name,
+//     request.files.file.data, function(err){
+//         if(err)
+//             console.log(err);
+//         response.send(request.files.file.name);
+//     })
+// })
 
 
 
